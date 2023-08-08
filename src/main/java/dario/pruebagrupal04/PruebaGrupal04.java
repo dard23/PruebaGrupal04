c4d685f6a765e4313c1960850a52ee4ffa32ecd7
dard23 <141675448+dard23@users.noreply.github.com>
2023-08-08 16:34:17 -0300

    Initial commit

---
+++ src/main/java/dario/pruebagrupal04/PruebaGrupal04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dario.pruebagrupal04;
+
+/**
+ *
+ * @author dard
+ */
+public class PruebaGrupal04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dario/pruebagrupal04/darioruizdiaz.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dario.pruebagrupal04;
+
+/**
+ *
+ * @author dard
+ */
+public class darioruizdiaz {
+    
+}
